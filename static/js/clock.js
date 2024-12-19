1c2c7de0a34d18d68b579aa2f5ab4ac947ee2e3d
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-12-19 16:39:48 +0900

    Add initial style

---
+++ static/js/clock.js
@@ -0,0 +1,5 @@
+const seconds_hand = document.querySelector(".seconds-hand");
+const hours_hand = document.querySelector(".hours-hand");
+const minutes_hand = document.querySelector(".minutes-hand");
+
+
